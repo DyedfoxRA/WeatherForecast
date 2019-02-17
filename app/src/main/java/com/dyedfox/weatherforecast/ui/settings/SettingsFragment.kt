339373b135ce944efc8580992821106186786e76
dyedfox <dyedfox.ra@gmail.com>
2019-02-17 21:19:26 +0100

    Initial commit

---
+++ app/src/main/java/com/dyedfox/weatherforecast/ui/settings/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.dyedfox.weatherforecast.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+}
---
+++ app/src/main/java/com/dyedfox/weatherforecast/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.dyedfox.weatherforecast.ui.weather.current
+
+import androidx.lifecycle.ViewModel;
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
