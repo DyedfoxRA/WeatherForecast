f5b027fc9eebc135ff6779ee66a717d28c0de377
dyedfox <dyedfox.ra@gmail.com>
2019-02-19 23:54:47 +0100

    Local DataBase Room and Dao

---
+++ app/src/main/java/com/dyedfox/weatherforecast/Internal/Exeptions.kt
@@ -0,0 +1,5 @@
+package com.dyedfox.weatherforecast.Internal
+
+import java.io.IOException
+
+class NoConnectivityExeption: IOException()
---
+++ app/src/main/java/com/dyedfox/weatherforecast/data/network/ConnectivityInterceptor.kt
@@ -0,0 +1,5 @@
+package com.dyedfox.weatherforecast.data.network
+
+import okhttp3.Interceptor
+
+interface ConnectivityInterceptor : Interceptor
